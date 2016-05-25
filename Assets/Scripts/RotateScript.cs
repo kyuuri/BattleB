d655a97358ecca867ceff19eb75c1541e167c960
kyuuri <dark_e2102@hotmail.com>
2016-05-26 00:41:42 +0700

    Add test ball.

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	public float xRotate = 0;
+	public float yRotate = 0;
+	public float zRotate = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3(xRotate * Time.deltaTime, yRotate * Time.deltaTime, zRotate * Time.deltaTime));
+	}
+}
