54ed5ef5d75ece9057703954cb3995ea64a8ba34
Aof Wat Wattanagaroon <wat.wattanagaroon@gmail.com>
2016-05-25 23:32:17 +0700

    First commit from network unity

---
+++ Assets/PlayerTest.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerTest : NetworkBehaviour {
+	public const int maxHealth = 100;
+
+	public bool destroyOnDeath;
+
+	public GameObject[] list;
+
+	[SyncVar]
+	public int currentHealth = maxHealth;
+
+	[SyncVar]
+	public int count = 0;
+
+	public RectTransform healthBar;
+
+	public void Start(){
+		list = GameObject.FindGameObjectsWithTag ("Player");
+	}
+
+	public void Update(){
+		if (isServer) {
+
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			list = GameObject.FindGameObjectsWithTag ("Player");
+			++count;
+		}
+	}
+
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+			return;
+
+		currentHealth -= amount;
+
+		if (currentHealth <= 0)
+		{
+			if (destroyOnDeath)
+			{
+				Destroy(gameObject);
+			} 
+			else
+			{
+				currentHealth = maxHealth;
+
+				// called on the Server, will be invoked on the Clients
+				RpcRespawn();
+			}
+		}
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{        if (isLocalPlayer)
+		{
+			// Set the player’s position to origin
+			transform.position = Vector3.zero;
+		}
+	}
+}
