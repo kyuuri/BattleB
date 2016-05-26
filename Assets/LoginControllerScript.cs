e5747cd076553f025d1335eb8a55222d9e6e572c
kokkoks <kok_satan@hotmail.com>
2016-05-26 21:49:19 +0700

    add http controller

--- Assets/LoginControllerScript.cs
+++ Assets/LoginControllerScript.cs
@@ -1,81 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class LoginControllerScript : MonoBehaviour {
-	private string url = "http://127.0.0.1:3000/users";
 	public InputField userName;
 	public InputField password;
 	public Button signinBtn;
 	public Button signupBtn;
+	public string name;
+	public int sc;
+
+	private List<string[]> allUsers;
+	private HttpControllerScript httpController;
 
 	private bool isExist;
 
 	// Use this for initialization
 	void Start () {
-	
+		httpController = new HttpControllerScript ();
+//		GetPlayers ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.U)){
+			allUsers = httpController.GetPlayers ();
+			ShowPlayers ();
+
+		} else if (Input.GetKeyDown(KeyCode.P)){
+			httpController.PutScore(name, sc);
+		}
 	
+	}
+
+	private void ShowPlayers(){
+		for (int i = 0; i < allUsers.Count; i++) {
+//			Debug.Log (i);
+			Debug.Log (allUsers [i] [1]);
+		}
 	}
 
 	public void LoginManagement(){
 		string user = userName.text;
 		string pw = password.text;
 
-		CheckExistingUser (user, pw);
-	}
-
-	private void CheckExistingUser(string user, string pw){
-		isExist = false;
-		string encryptPw = Md5Sum (pw);
-		string checkEncryptPw ="";
-
-		HTTP.Request someRequest = new HTTP.Request( "get", url);
-		someRequest.Send( ( request ) => {
-			// parse some JSON, for example:
-			JSONObject thing = new JSONObject( request.response.Text );
-			//			accessData(thing);
-			for(int i = 0; i < thing.list.Count; i++){
-				JSONObject j = thing.list[i];
-				JSONObject userName = j["UserName"];
-				if(user == userName.str){
-					isExist = true;
-					JSONObject pass = j["Password"];
-					checkEncryptPw = pass.str;
-				}
-			}
-			if(isExist){
-				if(encryptPw == checkEncryptPw){
-					Debug.Log("Logged In");
-				} else {
-					Debug.Log("Worng Password");
-				}
-			} else {
-				Debug.Log("Invalid User");
-			}
-		});
-	}
-
-	public  string Md5Sum(string strToEncrypt)
-	{
-		System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
-		byte[] bytes = ue.GetBytes(strToEncrypt);
-
-		// encrypt bytes
-		System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-		byte[] hashBytes = md5.ComputeHash(bytes);
-
-		// Convert the encrypted bytes back to a string (base 16)
-		string hashString = "";
-
-		for (int i = 0; i < hashBytes.Length; i++)
-		{
-			hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
-		}
-
-		return hashString.PadLeft(32, '0');
+		httpController.CheckExistingUser (user, pw);
 	}
 }
