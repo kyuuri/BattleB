fd2be5ec8dd6052c7819e861789dc40b3d80f3fa
Aof Wat Wattanagaroon <wat.wattanagaroon@gmail.com>
2016-05-27 14:22:50 +0700

    Create ranking

--- Assets/Scripts/Ranking.cs
+++ Assets/Scripts/Ranking.cs
@@ -6,13 +6,16 @@ public class Ranking : MonoBehaviour {
 	public GameObject[] allPlayer;
 	// Use this for initialization
 	void Start () {
-		allPlayer = GlobalData.allPlayers;
-		Debug.Log (allPlayer.Length);
-		for (int index = 0; index < 10; index++) {
-			PlayerController player = allPlayer [index].GetComponent<PlayerController> ();
-//			GameObject.Find ("Score (" + index + ")").GetComponent<Text> ().text;
-			Debug.Log(player.name+" = "+player.score);
-		}
+		int[] scoreList = GlobalData.allPlayerScore;
+		string[] nameList = GlobalData.allPlayersName;
+		Debug.Log (scoreList [0]);
+		Debug.Log (scoreList [1]);
+//		Debug.Log (allPlayer.Length);
+//		for (int index = 0; index < 10; index++) {
+//			PlayerController player = allPlayer [index].GetComponent<PlayerController> ();
+////			GameObject.Find ("Score (" + index + ")").GetComponent<Text> ().text;
+//			Debug.Log(player.name+" = "+player.score);
+//		}
 
 	
 	}
